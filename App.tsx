8d7e807ac095381affdf86fee65145a44a89599c
Gustavo Luiz <gustavodasilva0606@gmail.com>
2022-09-25 20:59:17 -0300

    Tela inicial, carregando as ligas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { View } from 'react-native'
+import Home from './src/screens/Home'
 
 const App = () => {
 
     return (
-        <View>
-        </View>
+        <Home/>
     )
 
 }
